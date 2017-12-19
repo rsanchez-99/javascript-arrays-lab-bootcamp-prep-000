6173bae4511d06d49c2fddaaec72db8e4acf9a55
Raul Sanchez <rsanchez_99@msn.com>
2017-12-19 03:50:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+   kittens.push(name);
+ }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  kittens = [...kittens, name]
+  return kittens
+}
